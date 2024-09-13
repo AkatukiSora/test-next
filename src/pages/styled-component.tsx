f6083d838a6c7c6e93e144baeaf4c380e9511169
暁月蒼空 <80885196+AkatukiSora@users.noreply.github.com>
2024-09-13 02:33:01 +0000

    styled-components

---
+++ src/pages/styled-component.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from "next";
+import Head from "next/head"
+import styled from "styled-components";
+
+const H1 = styled.h1`color: red;`
+
+const StyledComponent: NextPage = () => {
+
+    return <div>
+        <Head>
+            <title>styled components test</title>
+        </Head>
+        <main>
+            <H1>styled componentsのテスト</H1>
+        </main>
+    </div>
+}
+
+export default StyledComponent
